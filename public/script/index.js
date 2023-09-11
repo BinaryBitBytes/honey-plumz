14bc6971d3a9078833308016b5669180f2a31253
BinaryBitBytes <117mwc117@gmail.com>
2023-09-11 07:45:56 -0500

    init react app

---
+++ public/script/index.js
@@ -0,0 +1,28 @@
+// import
+//**FRONT END TECHNOLOGIES */
+//*BACK END TECHNOLOGIES */
+// @params 
+// import apollo from 'apollo-server' //*< import ___[PACKAGE]___ from '___________'
+// const { ApolloServer } = apollo //*< const {__NAME__} = ___[PACKAGE]___
+
+// construct
+
+// declare
+
+// test TRUE
+
+// test FALSE
+
+// Function #1
+// Interactive With User On Front End
+
+// Function #2.A
+// Light Mode
+
+// Function #2.B
+// Dark Mode
+
+// Function #3
+// Mouse Cursor Color Change //! WHITE < __  __ > MAGENTA
+
+//**DOCUMENTATION &&! USE CASE */
