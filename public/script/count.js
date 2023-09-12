29c3fa6f0fd08b0ee7ef6903b5a722efc4f7db66
BinaryBitBytes <117mwc117@gmail.com>
2023-09-11 22:07:00 -0500

    working on simple counting function in script/count.js also created a note of syntax and semantics for react usage called 'ReactUsageNotes.md'

---
+++ public/script/count.js
@@ -0,0 +1,11 @@
+const counting = (count) => {
+    const count = () => {
+    for(let count=0; count<=0; count++){ 
+        count = 31;
+        console.log(count(31));
+    }
+    return
+    }
+}
+counting(31);
+export default counting;
