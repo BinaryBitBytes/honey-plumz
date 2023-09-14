37711696a9f756a88d5be65f9ce1dc989722f4cc
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2023-09-14 02:15:29 -0400

    changed react component names to pascal case (upper case). Deleted index.css and installed bootsrap. In index.js removed the index.css and imported bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import nav from './components/nav';
-import Cards from './components/Products/cards';
-import Products from './components/Products/products';
+import Cards from './components/Products/Cards';
 import resetPass from './components/Authentication/resetPass';
+import CartButton from './components/Products/cartButton.jsx';
 
 function App() {
   return (
     <div className="App"> 
-    <>
-      <nav />
-    </>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, Welcome to HoneyPlumz
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
+        <p>
+          Hello, Welcome to HoneyPlumz
+        </p>
         Honey Plumz
         </a>
       </header>
---
+++ src/components/Products/CartButton.jsx
@@ -0,0 +1,7 @@
+export default function CartButton() {
+    return (
+        <>
+            <button>Cart</button>
+        </>
+    )
+}
