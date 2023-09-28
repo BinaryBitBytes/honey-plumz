e850104a8990b961de8517beeefecf5633b1b7c7
BinaryBitBytes <117mwc117@gmail.com>
2023-09-28 02:24:23 -0400

    updated changes to the pages of the src/components/frontend/ jsx elements

--- src/components/FrontEnd/NavBar.jsx
+++ src/components/FrontEnd/NavBar.jsx
@@ -1,31 +1,23 @@
 // Navbar.js
-import React from 'react';
-import { Link } from 'react-router-dom'; // If you're using React Router
+import React from "react";
+import { Link,  NavLink} from "react-router-dom"; // If you're using React Router
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
+      <nav>       
           <Link to="./pages/index.html">Home</Link>
-        </li>
-        <li>
+          <NavLink to="/about" activeClassName="active">Home</NavLink>
           <Link to="./pages/about.html">About</Link>
-        </li>
-        <li>
+          <NavLink to="/about" activeClassName="active">About</NavLink>
           <Link to="./pages/contact.html">Contact Us</Link>
-        </li>
-        <li>
+          <NavLink to="/about" activeClassName="active">Contact Us</NavLink>
           <Link to="./Products/products.jsx">Products</Link>
-        </li>
-        <li>
+          <NavLink to="/about" activeClassName="active">Products</NavLink>
           <Link to="./Log/cart.jsx">My Cart</Link>
-        </li>
-        <li>
+          <NavLink to="/about" activeClassName="active">My Cart</NavLink>
           <Link to="./Log/cart.jsx">Login</Link>
-        </li>
-      </ul>
-    </nav>
+          <NavLink to="/about" activeClassName="active">Login</NavLink>
+      </nav>
   );
 }
 
