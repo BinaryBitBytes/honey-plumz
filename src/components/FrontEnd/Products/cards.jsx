aef18f4c171c691aa2448bf45c3e532e7fe471e0
BinaryBitBytes <117mwc117@gmail.com>
2023-09-28 14:44:49 -0400

    added error boundry to cards.jsx

--- src/components/FrontEnd/Products/cards.jsx
+++ src/components/FrontEnd/Products/cards.jsx
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from "react";
 import vapeImages from "../pics/vapeImages.js";
 
+// Define an ErrorBoundary component for error handling
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({ hasError: true });
+    // You can log the error or send it to an error tracking service here
+    console.error(error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render a fallback UI when an error occurs
+      return <div>Something went wrong.</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
 // cards are used for the products
 // each card will access a file in the public/style folder
-// each product is sub-categorized in its respected folder based on chemical makup
+// each product is sub-categorized in its respected folder based on chemical makeup
 function Cards() {
   const [images, setImages] = useState(vapeImages);
 
@@ -28,4 +51,11 @@ function Cards() {
   );
 }
 
-export default Cards;
+// Wrap your Cards component with the ErrorBoundary
+export default function CardsWithBoundary() {
+  return (
+    <ErrorBoundary>
+      <Cards />
+    </ErrorBoundary>
+  );
+}
