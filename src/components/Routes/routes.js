33a5cd82bdbd588deb2b568e0de6700dacf82a96
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2023-10-04 00:14:56 -0400

    on routes.js made changes to the function by renaming it because its name took place of the imported function from react-router-dom as the name Route. Also due to V6 changes the Switch function is deprecated. Changed from Swith to Routes. Also imported the Route component

--- src/components/Routes/routes.js
+++ src/components/Routes/routes.js
@@ -1,5 +1,5 @@
+import { Routes, Route } from 'react-router-dom';
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
 import Home from '../FrontEnd/pages/Home.jsx'; // Import your Home component
 import About from '../FrontEnd/pages/About.jsx'; // Im                                                                                                                                                                                                                                                                                           port your About component
 import Contact from '../FrontEnd/pages/Contact.jsx'; // Import your Contact component
@@ -7,17 +7,28 @@ import Products from '../FrontEnd/Products/Products.jsx'; // Import your Product
 import Cart from '../FrontEnd/Log/Cart.jsx'; // Import your Cart component
 import Login from '../FrontEnd/Log/LogIn.jsx'; // Import your Login component
 
-function Routes() {
+function Routing() {
   return (
-    <Switch>
+    <Routes>
       <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/products" component={Products} />
-      <Route path="/cart" component={Cart} />
-      <Route path="/login" component={Login} />
-    </Switch>
+      <Route path="about" component={About} />
+      <Route path="contact" component={Contact} />
+      <Route path="products" component={Products} />
+      <Route path="cart" component={Cart} />
+      <Route path="login" component={Login} />
+    </Routes>
   );
 }
 
-export default Routes;
+export default Routing;
+
+// *Before changes
+
+  // <Switch>
+  //     <Route exact path="/" component={Home} />
+  //     <Route path="about" component={About} />
+  //     <Route path="contact" component={Contact} />
+  //     <Route path="products" component={Products} />
+  //     <Route path="cart" component={Cart} />
+  //     <Route path="login" component={Login} />
+  // </Switch>
